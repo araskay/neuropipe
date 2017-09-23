56a7fa424a96100f2d3d6b21642388711c72bd30
Aras <mkayvan@gmail.com>
2017-09-22 22:34:16 -0400

    ver 1.0.6

--- skullstripstructural.py
+++ skullstripstructural.py
@@ -2,11 +2,12 @@ import workflow, getopt,sys,fileutils,shutil,os, subprocess
 
 ifile=''
 ofile=''
+skiprecon=False
 
 # parse command-line arguments
 try:
     (opts,args) = getopt.getopt(sys.argv[1:],'hi:o:',\
-                                ['help','input=', 'output='])
+                                ['help','input=', 'output=', 'skiprecon'])
 except getopt.GetoptError:
     print('usage: makeconnseed.py -i <input subject file> -o <output subject file>')
     sys.exit()
@@ -18,6 +19,8 @@ for (opt,arg) in opts:
         ifile=arg
     elif opt in ('-o','--output'):
         ofile=arg
+    elif opt in ('--skiprecon'):
+        skiprecon=True
 
 if ifile=='' or ofile=='':
     sys.exit('Please provide both input subject file and output subject file')
@@ -29,8 +32,9 @@ for subj in subjects:
         for run in sess.runs:
             (opath,oname)=os.path.split(run.data.structural)
             fileutils.createdir(opath)
-            p=subprocess.Popen(['recon-all','-sd',opath,'-subjid','__recon-all','-i',run.data.structural,'-autorecon1'])
-            p.communicate()
+            if not skiprecon:
+                p=subprocess.Popen(['recon-all','-sd',opath,'-subjid','__recon-all','-i',run.data.structural,'-autorecon1'])
+                p.communicate()
             p=subprocess.Popen(['mri_convert',opath+'/__recon-all/mri/brainmask.mgz',\
                                 opath+'/'+fileutils.removext(oname)+'_brainmask.nii.gz'])
             p.communicate()
