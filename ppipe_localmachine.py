db5ba444b54da7ca1ae841cf4c55a33244978611
aras (chegs) <arasmedim@gmail.com>
2018-08-07 11:11:39 -0400

    v1.61 ppipe_localmachnie.py updated

--- ppipe_localmachine.py
+++ ppipe_localmachine.py
@@ -9,6 +9,9 @@ Created on Thu May  3 13:48:29 2018
 def printhelp():
     p=subprocess.Popen(['pipe.py','-h'])
     p.communicate()
+    print('---------------------------------------')
+    print('Additional parallel processing options:')
+    print('[--numpar <number of parallel jobs = 16>]')
 
 import workflow
 import preprocessingstep
@@ -36,6 +39,8 @@ runpipe=False
 
 envvars=workflow.EnvVars()
 
+numpar=16
+
 # the getopt libraray somehow "guesses" the arguments- for example if given
 # '--subject' it will automatically produce '--subjects'. This can cause problems
 # later when arguments from sys.argv are passed to pipe.py. The following checks
@@ -50,7 +55,7 @@ for arg in pipe_args:
                        '--tomni', '--boldregdof', '--structregdof',\
                        '--boldregcost', '--structregcost', '--outputsubjects',\
                        '--keepintermed', '--runpipename', '--fixpipename',\
-                       '--optpipename']:
+                       '--optpipename','--numpar']:
             printhelp()
             sys.exit()
 
@@ -61,7 +66,7 @@ subjfile=''
 # parse command-line arguments
 try:
     (opts,args) = getopt.getopt(sys.argv[1:],'h',\
-                                ['help','pipeline=', 'subjects=', 'perm=', 'onoff=', 'permonoff=', 'const=', 'select=', 'add', 'combine', 'fixed=', 'showpipes', 'template=', 'resout=', 'parcellate', 'meants', 'seedconn', 'tomni', 'boldregdof=', 'structregdof=', 'boldregcost=', 'structregcost=', 'outputsubjects=', 'keepintermed', 'runpipename=', 'fixpipename=', 'optpipename='])
+                                ['help','pipeline=', 'subjects=', 'perm=', 'onoff=', 'permonoff=', 'const=', 'select=', 'add', 'combine', 'fixed=', 'showpipes', 'template=', 'resout=', 'parcellate', 'meants', 'seedconn', 'tomni', 'boldregdof=', 'structregdof=', 'boldregcost=', 'structregcost=', 'outputsubjects=', 'keepintermed', 'runpipename=', 'fixpipename=', 'optpipename=','numpar='])
 except getopt.GetoptError:
     printhelp()
     sys.exit()
@@ -118,6 +123,8 @@ for (opt,arg) in opts:
     elif opt in ('--subjects'):
         subjectsfiles.append(arg)
         (directory,subjfile)=os.path.split(arg) # this is inconsistent with the idea of having multiple subject files- maybe just get one subjects file?
+    elif opt in ('--numpar'):
+        numpar=int(arg)
 
 if subjectsfiles==[]:
     sys.exit('Please specify subjects file. Get help using -h or --help.')
@@ -127,8 +134,9 @@ base_command = 'pipe.py'
 if runpipe:
 
     subjects=[]
-    count=0
 
+    count=0
+    proccount=0
     processes = []
 
     for sfile in subjectsfiles:
@@ -138,6 +146,8 @@ if runpipe:
             # first create individual subjects files and job bash scripts to be
             # submitted to the job manager
             count+=1
+            proccount+=1
+
             subject_fname = '.temp_subj_'+subjfile+'_'+runpipefile+str(count)+'.txt'
             
             command=[]
@@ -157,12 +167,23 @@ if runpipe:
 
             pipe_args[pipe_args.index('--subjects')+1] = subject_fname
 
+            if '--numpar' in pipe_args:
+                del pipe_args[pipe_args.index('--numpar')+1]
+                del pipe_args[pipe_args.index('--numpar')]
+
             command = command+ pipe_args
             
             # now submit job
             print('Running',' '.join(command))
             p=subprocess.Popen(command,stdout=f_o,stderr=f_e)
             processes.append(p)
+
+            if proccount==numpar:
+                for p in processes:
+                    p.wait()
+                proccount=0
+                processes=[]
+                print('Total of',count,'jobs done')
 
     for p in processes:
         p.wait()            
