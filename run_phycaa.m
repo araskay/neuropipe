b391718b48e312f14669e6fdf5b738ae96523ff2
aras (chegs) <arasmedim@gmail.com>
2018-08-20 15:29:15 -0400

    v1.65

--- run_phycaa.m
+++ run_phycaa.m
@@ -8,4 +8,12 @@ function run_phycaa(bold,brainmask,TR,out_prefix)
     dataInfo.TR=TR;
     dataInfo.make_output=1;
 
-    run_PHYCAA_plus(input_cell, brainmask, [], [], dataInfo, 2, out_prefix);
+    output2 = run_PHYCAA_plus(input_cell, brainmask, [], [], dataInfo, 2, out_prefix);
+    % output2 is the output of step2
+
+    % also save noise components
+    
+    for i=1:size(output2.Physio_Tset,1)
+        dlmwrite(strcat(out_prefix,'_noisecomp_split',int2str(i),'.csv'),output2.Physio_Tset{i})
+    end
+    
