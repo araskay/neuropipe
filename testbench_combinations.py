c6b741e1c46bd719123ba5d05ab1931c9591a51b
Aras Kayvanrad <mkayvan@gmail.com>
2019-03-24 18:47:30 -0400

    removed testbench*.py

--- testbench_combinations.py
+++
@@ -1,9 +0,0 @@
-import preprocessingstep as p
-l1=[1,2]
-l2=[]
-a=list(p.permutations(l1))
-print(a)
-b=list(p.onoff(l2))
-print(b)
-c=p.concatstepslists(a,b)
-print(list(c))
--- testbench_seedcorr.py
+++
@@ -1,7 +0,0 @@
-import seedcorr
-
-ifile='/home/mkayvanrad/code/pipeline/temp/mbepi.nii.gz'
-ofile='/home/mkayvanrad/code/pipeline/temp/r.nii.gz'
-seedfile='/home/mkayvanrad/code/pipeline/temp/pcc_harvard-oxford_mbepispace.nii.gz'
-
-seedcorr.calcseedcorr(ifile, seedfile, ofile)
